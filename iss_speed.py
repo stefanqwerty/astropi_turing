2b20ec3e61e015d1d1d9e5c9b5e98caab2f0b763
Vlad79X <109083165+Vlad79X@users.noreply.github.com>
2024-02-15 16:33:28 +0200

    Add files via upload

--- iss_speed.py
+++ iss_speed.py
@@ -2,8 +2,12 @@ from exif import Image
 from datetime import datetime
 import math
 from decimal import *
+from picamera import PiCamera
+
+#!MAXIM 5 CIFRE, DE CITIT DOCUMENTATIA
 
 def decimal_coords(coords, ref):
+    
     decimal_degrees = Decimal(coords[0] + coords[1] / 60 + coords[2] / 3600)
     if ref == "S" or ref =='W' :
         decimal_degrees = -decimal_degrees
@@ -27,12 +31,10 @@ def image_coordinates(image_path):
         
     return(coords[0])
 
-#print(image_coordinates('photo_0683.jpg'))
-
 a=Decimal(6378.1370)
 b=Decimal(6357.7523)
-
 def earthRadius(lat):
+    
     phi=Decimal(lat)
     cosine=Decimal(math.cos(phi))
     sine=Decimal(math.sin(phi))
@@ -46,11 +48,44 @@ def earthRadius(lat):
 g=Decimal(6.673e-20)
 m=Decimal(5.972e24)
 def calcSpeed(radius, distance):
+    
     r=radius+distance
     speed=Decimal(math.sqrt((g*m)/r))
+    
     return speed
 
-for x in range(1, 55):
-    s='photo ('+ str(x)+').jpg'
-    print(calcSpeed(earthRadius(image_coordinates(s)), 403))
+def takePicture():
     
+    s=imgName
+    print(calcSpeed(earthRadius(image_coordinates(s)), 403))
+
+def getHeight():
+    return 403
+
+def calcImage(imgName):
+    
+    s='photo ('+ str(x)+').jpg'
+    height = getHeight()
+    calcSpeed(earthRadius(image_coordinates(s)), height)
+ 
+ 
+def printOutput(speed):
+    
+    f = open("result.txt", "w") 
+    rounded = "{:.4f}".format(speed)
+    f.write(str(rounded))
+    f.close()
+
+def Program():
+    cam = PiCamera()
+    cam.resolution = (4056, 3040)
+    for i in range(1,40):
+        s='photo ('+ str(i)+').jpg'
+        print(s)
+        cam.capture(s)
+        Sleep(400)
+    #  fapoza()
+    #  calculeazadinpoza()
+        sleep(1e4)
+    #
+    #afiseaza()
